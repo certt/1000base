54f84b530528903f97bfee265acc444b351b996b
Kamil <kamil531@gmail.com>
2018-03-05 21:54:24 +0100

    Fix double original CM call
    quick fix, noticed it

--- 1000base/CreateMove.cpp
+++ 1000base/CreateMove.cpp
@@ -3,10 +3,10 @@
 CreateMoveFn oCreateMove;
 bool __stdcall Hooks::CreateMove(float flInputSampleTime, CUserCmd* pCmd)
 {
-	oCreateMove(flInputSampleTime, pCmd); // Cause that`s fix a lot of bugs w/ this hook
+	auto bCreateMoveReturn = oCreateMove(flInputSampleTime, pCmd); // Cause that`s fix a lot of bugs w/ this hook
 
 	if (!pCmd || !pCmd->command_number)
-		return oCreateMove(flInputSampleTime, pCmd);
+		return bCreateMoveReturn;
 
 	g_Globals->LocalPlayer = I::EntityList->GetClientEntity(I::Engine->GetLocalPlayer());
 
